267d3617f94e39a9f94fe37d8cf7afdbbd2fbe3e
LostMind62 <48745381+LostMind62@users.noreply.github.com>
2026-03-12 08:57:26 -0600

    Add text field for number input in counter example
    adding space for identation

--- counter.py
+++ counter.py
@@ -5,6 +5,9 @@ def main(page: ft.Page):
     page.title = "Flet counter example"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
+
+    
+
     txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
     def minus_click(e):
